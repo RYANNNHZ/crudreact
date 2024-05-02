9c708e17dc28f610f4409c4b5a51bf579e486063
RYANNNHZ <riantojelek871@gmail.com>
2024-05-02 15:50:05 +0700

    second commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -30,7 +30,6 @@ export default function(){
 
         
     },[]);
-    //perbaiki fungsi insert siswa
     async function InsertSiswa(){
         const res = await fetch(`http://127.0.0.1:8000/api/v1/store`,{
             method : 'POST',
